42e78bddbe836ce010312faacb4e704bd3dcef8c
yam <yamil.yscapa@gmail.com>
2025-12-01 21:47:31 -0600

    feat/add http logger

--- apps/auth/src/index.ts
+++ apps/auth/src/index.ts
@@ -2,10 +2,17 @@ import "dotenv/config";
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { router } from "./router";
+import { logger } from "hono/logger";
 
 const app = new Hono();
+
 const port: number = Number(process.env.PORT || 5001);
+
+// Routes
 app.route("/", router);
+
+// Middleware
+app.use(logger());
 
 serve({
   fetch: app.fetch,
